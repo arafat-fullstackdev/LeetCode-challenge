108fdcb4b5e13055883bc3da319cd6ab18e230ba
Arafat Patwary <farabipsk@gmail.com>
2025-08-10 19:20:19 +0600

    use vector

--- starter/practice.cpp
+++ starter/practice.cpp
@@ -3,7 +3,9 @@
 #include <algorithm>
 using namespace std;
 
-
+void my_swap(){
+    int m,n;
+}
 int main() {
     int a,b,c,d;
     cin>>a>>b>>c>>d;
@@ -13,5 +15,30 @@ int main() {
 
     cout<<mn<<" "<<mx<<endl;
     
+    return 0;
+}
+
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+void printVector(vector<int>& v) {
+    for (auto x: v) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+int main() {
+
+    // Creating a vector of 5 elements from
+    // initializer list
+    vector<int> v1 = {1, 4, 2, 3, 5};
+
+    // Creating a vector of 5 elements with
+    // default value
+    vector<int> v2(5, 9);
+
+    printVector(v1);
+    printVector(v2);
     return 0;
 }
