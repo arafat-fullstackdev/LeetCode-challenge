fdd6ab78f5bf3c4d61297633de2437c339024027
Arafat Patwary <farabipsk@gmail.com>
2025-08-18 19:50:48 +0600

    Implement Square pattern

--- practice/pattern.cpp
+++ practice/pattern.cpp
@@ -11,16 +11,34 @@ int main() {
     //     }
     //     cout<<endl;
     // }
-    for (int i = 0; i < n; i++)
-    {
-        char ch = 'A';
-       for (int  j = 0; j < n; j++)  //line start
-       {
-        cout<<ch<<" ";
-        ch = ch +1;  // char + int
-       }
-       cout<<endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A';
+    //    for (int  j = 0; j < n; j++)  //line start
+    //    {
+    //     cout<<ch<<" ";
+    //     ch = ch +1;  // char + int
+    //    }
+    //    cout<<endl;
        
+    // }
+
+    //int num =1;
+    // for(int i =0; i<n; i++){
+    //     for(int k =0; k<n; k++){
+    //      cout<<num<<" ";
+    //      num++;
+    //     }
+    //     cout<<endl;
+    // }
+    char chart = 'A';
+    for(int i =0; i<n; i++){
+        for(int j =0; j<n; j++){
+
+            cout <<chart<<" ";
+            chart ++;
+        }
+        cout<<endl;
     }
     
     
