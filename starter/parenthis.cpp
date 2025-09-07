1efbe892d6646510ab89235b6c3136749908d19f
Arafat Patwary <farabipsk@gmail.com>
2025-09-07 20:41:26 +0600

    String char

--- starter/parenthis.cpp
+++ starter/parenthis.cpp
@@ -46,4 +46,27 @@ public:
 
 //   // Output the memory address of myAge with the pointer (0x7ffe5367e044)
 //   cout("%p\n", ptr);
+
+// Find string char
+ std::string my_string = "Hello, world!";
+    char char_to_find = 'o';
+
+    // Find the first occurrence of 'o'
+    size_t found_pos = my_string.find(char_to_find);
+
+    // Check if the character was found
+    if (found_pos != std::string::npos) {
+        std::cout << "Character '" << char_to_find << "' found at position: " << found_pos << std::endl;
+    } else {
+        std::cout << "Character '" << char_to_find << "' not found." << std::endl;
+    }
+
+    // Find a character that doesn't exist in the string
+    char non_existent_char = 'z';
+    size_t not_found_pos = my_string.find(non_existent_char);
+
+    if (not_found_pos == std::string::npos) {
+        std::cout << "Character '" << non_existent_char << "' not found, as expected." << std::endl;
+    }
+    
 };
